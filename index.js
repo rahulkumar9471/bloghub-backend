0a65618bbd8590100eb2267a0f38b6ec8c5c3749
Rahul Kumar <rk8717989@gmail.com>
2024-04-04 07:37:35 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log('app listening on port ' + PORT);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
